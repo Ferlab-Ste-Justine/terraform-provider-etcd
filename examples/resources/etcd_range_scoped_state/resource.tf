e6cc709e35fd29b5d705f761e6e00bab7e4a1e95
Eric Vallee <eric_vallee@webificservices.com>
2022-06-07 14:31:10 -0400

    feat: INFRA-24 Added example for new resource

---
+++ examples/resources/etcd_range_scoped_state/resource.tf
@@ -0,0 +1,11 @@
+data "etcd_prefix_range_end" "patroni" {
+  provider = etcd.patroni
+  key = "/patroni/"
+}
+
+resource "etcd_range_scoped_state" "patroni" {
+    key = data.etcd_prefix_range_end.patroni.key
+    range_end = data.etcd_prefix_range_end.patroni.range_end
+    clear_on_creation = false
+    clear_on_deletion = true
+}
